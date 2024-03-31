9d5b46b5d002f3eb022d1043c4bcc36ade3687a5
Ana del Carpio <anadelcarpiov@gmail.com>
2024-03-31 00:09:01 +0000

    corregido

--- ex05/ft_print_comb.c
+++ ex05/ft_print_comb.c
@@ -6,10 +6,10 @@ void ft_print_comb(void)
     int c2;
     int c3;
 
-    while (c1<=55)  
+    while (c1<=57)  
     {
         c2=c1+1; 
-        while(c2<=56) 
+        while(c2<=57) 
         {
             c3=c2+1;
             while(c3<=57)
@@ -17,13 +17,18 @@ void ft_print_comb(void)
                 write(1,&c1,1); 
                 write(1,&c2,1); 
                 write(1,&c3,1);
+                if (c1==55 && c2==56 && c3==57);
+                { 
+                return;
+                }
+                
                 write(1,",",1);
                 write(1," ",1);
 
                 c3++;
             }
-        c2++; // c2 = 58
+        c2++; 
         }
-    c1++; //c1=50
+    c1++; 
     }
 } 
